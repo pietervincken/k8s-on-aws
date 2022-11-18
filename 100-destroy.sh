98ac12c0ed8b644b40572aa12c9a0b78844e5bb2
pietervincken <4153479+pietervincken@users.noreply.github.com>
2022-11-18 17:35:33 +0100

    fix: optimize delete

--- 100-destroy.sh
+++ 100-destroy.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
+
+output=$(aws cloudformation describe-stacks --stack-name renovatetalkstate)
+bucketname=$(echo $output | jq --raw-output '.Stacks[0].Outputs[] | select(.OutputKey=="bucketname") | .OutputValue')
+
+# Delete all records in bucket, otherwise delete will fail
+# TF leaves "empty" tf statefile 
+aws s3 rm s3://$bucketname --recursive
+
 aws cloudformation delete-stack \
     --stack-name renovatetalkstate
