c343fd2b1834092e5b9449296c60396c9ae7ac50
pietervincken <4153479+pietervincken@users.noreply.github.com>
2023-01-13 17:38:25 +0100

    automate more

--- 4-upload-app-image.sh
+++ 4-upload-app-image.sh
@@ -5,12 +5,18 @@ temp=$(mktemp -d)
 
 cd $temp
 
+ecr_repo_url=$(cat terraform/output.json| jq --raw-output '.ecr_repo_url.value' | sed 's|/.*||')
+if [ -z $ecr_repo_url ]; then
+    echo "Could not find ecr_repo_url. Stopping!"
+    exit 1
+fi
+
 git clone https://github.com/pietervincken/renovate-talk-java-demo-app.git
 cd renovate-talk-java-demo-app
 latest_tag=$(git tag | sort -V | tail -1 | sed 's|v||') # remove v from tag name!
 echo "Using tag $latest_tag"
 
-docker build --build-arg=APP_VERSION=$latest_tag -t 930970667460.dkr.ecr.eu-west-1.amazonaws.com/renovate-talk-java-demo-app:$latest_tag .
-aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 930970667460.dkr.ecr.eu-west-1.amazonaws.com
+docker build --build-arg=APP_VERSION=$latest_tag -t ecr_repo_url/renovate-talk-java-demo-app:$latest_tag .
+aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin ecr_repo_url
 
-docker push 930970667460.dkr.ecr.eu-west-1.amazonaws.com/renovate-talk-java-demo-app:$latest_tag
+docker push ecr_repo_url/renovate-talk-java-demo-app:$latest_tag
