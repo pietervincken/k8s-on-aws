5c82498840aae9f1038721e5ca0110dbbfa7fef3
pietervincken <4153479+pietervincken@users.noreply.github.com>
2023-01-31 15:49:05 +0100

    update delete application script

--- 97-delete-applications.sh
+++ 97-delete-applications.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-kubectl delete -k ../renovate-tekton-argo-talk/k8s/traefik
+kubectl delete pipelineruns --all -A
+kubectl delete pipelines --all -A
+kubectl delete tasks --all -A
+
+kubectl patch app traefik  -p '{"metadata": {"finalizers": ["resources-finalizer.argocd.argoproj.io"]}}' --type merge -n argocd
+kubectl delete app traefik -n argocd
+
+apps=$(kubectl get application -n argocd -o name | xargs -I{} basename {})
+
+
+for app in $apps
+do
+    kubectl patch app $app  -p '{"metadata": {"finalizers": ["resources-finalizer.argocd.argoproj.io"]}}' --type merge -n argocd
+    kubectl delete app $app -n argocd --wait=false
+done
+
+kubectl delete -k ../renovate-tekton-argo-talk/k8s/argocd
